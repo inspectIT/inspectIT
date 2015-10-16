9f94a076b9cb291ecd759b81c96013fda96b5ecf
Joshua Hartmann <zxmcr70@mn040001-c0018.uni-tuebingen.de>
2015-10-16 11:52:41 +0200

    bugfix: multiple hashing of password
    Retrieving a persisted user lead to hashing of the already hashed
    password

--- CMR/src/info/novatec/inspectit/cmr/usermanagement/User.java
+++ CMR/src/info/novatec/inspectit/cmr/usermanagement/User.java
@@ -1,15 +1,13 @@
 package info.novatec.inspectit.cmr.usermanagement;
 
-
-
-
 /**
  * Representing a user in the system.
+ * 
  * @author Joshua Hartmann
  *
  */
 public class User {
-	
+
 	/**
 	 * The unique name of the user.
 	 */
@@ -19,59 +17,68 @@ public class User {
 	 */
 	private String password;
 	/**
-	 * The users email adress.
-	 * Used to send a new password, if the current one is lost.
+	 * The users email adress. Used to send a new password, if the current one is lost.
 	 */
 	private String email;
 	/**
 	 * The id of the role the user is set to.
 	 */
 	private long roleId;
-	
+
 	/**
 	 * The default-constructor.
 	 */
 	public User() {
-		
+
 	}
+
 	/**
 	 * The constructor for a User object.
-	 * @param name The name of the user
-	 * @param password The hashed password of the user
-	 * @param email The email of the user
-	 * @param roleId The id of the role the user is attached to
+	 * 
+	 * @param name
+	 *            The name of the user
+	 * @param password
+	 *            The <b>hashed</b> password of the user
+	 * @param email
+	 *            The email of the user
+	 * @param roleId
+	 *            The id of the role the user is attached to
 	 */
 	public User(String name, String password, String email, long roleId) {
 		super();
 		this.name = name;
-		this.password = Permutation.hashString(password);
+		this.password = password;
 		this.email = email;
 		this.roleId = roleId;
 	}
+
 	/**
 	 * Gets {@link #name}.
-	 *   
-	 * @return {@link #name}  
+	 * 
+	 * @return {@link #name}
 	 */
 	public String getName() {
 		return name;
 	}
+
 	/**
 	 * Gets {@link #password}.
-	 *   
-	 * @return {@link #password}  
+	 * 
+	 * @return {@link #password}
 	 */
 	public String getPassword() {
 		return password;
 	}
+
 	/**
 	 * Gets {@link #email}.
-	 *   
-	 * @return {@link #email}  
+	 * 
+	 * @return {@link #email}
 	 */
 	public String getEmail() {
 		return email;
 	}
+
 	/**
 	 * {@inheritDoc}
 	 */
@@ -79,54 +86,55 @@ public class User {
 	public String toString() {
 		return "User [name=" + name + ", hashed password [" + password + "] , email=" + email + ", userRoleId=" + roleId + "]";
 	}
-	
-	
-	
-	/**  
-	 * Sets {@link #name}.  
-	 *   
-	 * @param name  
-	 *            New value for {@link #name}  
+
+	/**
+	 * Sets {@link #name}.
+	 * 
+	 * @param name
+	 *            New value for {@link #name}
 	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-	/**  
-	 * Sets {@link #password}.  
-	 *   
-	 * @param password  
-	 *            New value for {@link #password}
+
+	/**
+	 * Sets {@link #password}.
+	 * 
+	 * @param password
+	 *            New <b>hashed</b> value for {@link #password}
 	 */
 	public void setPassword(String password) {
-			this.password = Permutation.hashString(password);
+		this.password = password;
 
 	}
-	/**  
-	 * Sets {@link #email}.  
-	 *   
-	 * @param email  
-	 *            New value for {@link #email}  
+
+	/**
+	 * Sets {@link #email}.
+	 * 
+	 * @param email
+	 *            New value for {@link #email}
 	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	/**
 	 * Gets {@link #roleId}.
-	 *   
-	 * @return {@link #roleId}  
+	 * 
+	 * @return {@link #roleId}
 	 */
 	public long getRoleId() {
 		return roleId;
 	}
-	/**  
-	 * Sets {@link #roleId}.  
-	 *   
-	 * @param roleId  
-	 *            New value for {@link #roleId}  
+
+	/**
+	 * Sets {@link #roleId}.
+	 * 
+	 * @param roleId
+	 *            New value for {@link #roleId}
 	 */
 	public void setRoleId(long roleId) {
 		this.roleId = roleId;
 	}
-	
-	
+
 }
