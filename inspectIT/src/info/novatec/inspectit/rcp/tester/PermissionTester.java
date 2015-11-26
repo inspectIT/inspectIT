424c3bea39ebbae0061da1d5fe6886e3a5e29665
Lucca Hellriegel <lucca.hellriegel@gmx.net>
2015-11-26 19:52:16 +0100

    Fix of merging bug

--- inspectIT/src/info/novatec/inspectit/rcp/tester/PermissionTester.java
+++ inspectIT/src/info/novatec/inspectit/rcp/tester/PermissionTester.java
@@ -6,10 +6,8 @@ import org.eclipse.core.expressions.PropertyTester;
 
 import info.novatec.inspectit.rcp.provider.ICmrRepositoryAndAgentProvider;
 import info.novatec.inspectit.rcp.provider.ICmrRepositoryProvider;
-import info.novatec.inspectit.rcp.provider.IInputDefinitionProvider;
 import info.novatec.inspectit.rcp.provider.IStorageDataProvider;
 import info.novatec.inspectit.rcp.repository.CmrRepositoryDefinition;
-import info.novatec.inspectit.rcp.repository.RepositoryDefinition;
 
 /**
  * Tester for Permissions. Searchs the grantedPermissions list and gives an corresponding boolean.
@@ -30,25 +28,18 @@ public class PermissionTester extends PropertyTester {
 			cmrRepositoryDefinition = ((ICmrRepositoryAndAgentProvider) receiver).getCmrRepositoryDefinition();
 		} else if (receiver instanceof IStorageDataProvider) {
 			cmrRepositoryDefinition = ((IStorageDataProvider) receiver).getCmrRepositoryDefinition();
-		} else if (receiver instanceof IInputDefinitionProvider) {
-			RepositoryDefinition repository = ((IInputDefinitionProvider) receiver).getInputDefinition()
-					.getRepositoryDefinition();
-			if (repository instanceof CmrRepositoryDefinition) {
-				cmrRepositoryDefinition = (CmrRepositoryDefinition) repository;
-			} else {
-				return false;
-			}
 		} else {
 			return false;
 		}
-		
+
 		List<String> grantedPermissions = cmrRepositoryDefinition.getGrantedPermissions();
-		
+
 		try {
-		return grantedPermissions.contains(property);
-		} catch (NullPointerException lo) { 
-			return false; 
+			return grantedPermissions.contains(property);
+		} catch (NullPointerException lo) {
+			return false;
 		}
 	}
 
 }
+
