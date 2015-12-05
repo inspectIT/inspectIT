a864ca5bfcf9170e8aeb2faca106735008350052
Joshua Hartmann <jhartmann1995@gmail.com>
2015-12-05 16:17:07 +0100

    Added some admin module methods

--- CommonsCS/src/info/novatec/inspectit/cmr/service/ISecurityService.java
+++ CommonsCS/src/info/novatec/inspectit/cmr/service/ISecurityService.java
@@ -11,6 +11,8 @@ import info.novatec.inspectit.communication.data.cmr.User;
  * 
  * @author Andreas Herzog
  * @author Clemens Geibel
+ * @author Lucca Hellriegel
+ * @author Joshua Hartmann
  */
 @ServiceInterface(exporter = ServiceExporterType.HTTP)
 public interface ISecurityService {	
@@ -22,21 +24,33 @@ public interface ISecurityService {
 	 */
 	List<String> authenticate(String pw, String email);
 	
+	// | ROLE | --------------
 	/**
-	 * Returns a Role object with given Email.
+	 * Returns a Role object with given Email of the user.
 	 * @param email email
-	 * @return a Role object with given Email.
+	 * @return a Role object with given Email of the user.
 	 * @throws AuthenticationException if the email was not found.
 	 */
-	Role retrieveRole(String email);
-	
+	Role getRoleOfUser(String email);	
 	/**
 	 * Searches for the Role matching a given ID.
 	 * @param id RoleID.
 	 * @return Role with this ID.
 	 */
 	Role getRoleByID(long id);
+	/**
+	 * Retrieves all existing roles.
+	 * @return An Array containing all Roles
+	 */
+	Role[] getAllRoles();
 	
+	
+	// | USER |---------------
+	/**
+	 * We only want to send the user emails to the client. If a user is about to be modified, other data will be retrieved.
+	 * @return An Array containing all user emails
+	 */
+	String[] getAllUsers();
 	/**
 	 * Adds a new User to the Database.
 	 * Throws an exception, if there is an existing registered User with the given email-address.
@@ -44,6 +58,12 @@ public interface ISecurityService {
 	 * @param user user
 	 */
 	void addUser(User user);
+	/**
+	 * Returns the user object with the given email.
+	 * @param email Email address of the user.
+	 * @return The user object.
+	 */
+	User getUser(String email);
 	
 	/**
 	 * Deletes the given User Object from the Database.
@@ -58,10 +78,18 @@ public interface ISecurityService {
 	 */
 	void changeUserAttribute(User user);
 	
+	
+	// | PERMISSION |---------
 	/**
 	 * Change the description of a Permission.
 	 * Other changes should not be possible.
 	 * @param permission permission
 	 */
 	void changePermissionDescription(Permission permission);
+	
+	/**
+	 * Retrieves all existing permissions.
+	 * @return An Array containing all Roles
+	 */
+	Permission[] getAllPermissions();
 }
