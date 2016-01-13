00d17dcf3963e035bddcfd16de041a7d6253b428
MarioRose <mario.rose@rocketmail.com>
2016-01-13 19:03:05 +0100

    Show All Users and Roles

---
+++ inspectIT/src/info/novatec/inspectit/rcp/dialog/EditRoleDialog.java
@@ -0,0 +1,157 @@
+package info.novatec.inspectit.rcp.dialog;
+
+import info.novatec.inspectit.communication.data.cmr.Role;
+import info.novatec.inspectit.communication.data.cmr.User;
+import info.novatec.inspectit.rcp.repository.CmrRepositoryDefinition;
+import info.novatec.inspectit.rcp.wizard.page.CmrLoginWizardPage;
+
+import java.util.List;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.jface.dialogs.IMessageProvider;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * Dialog for resetting password in case Button in {@link CmrLoginWizardPage} is pressed.
+ * 
+ * @author Mario Rose
+ *
+ */
+
+public class EditRoleDialog extends TitleAreaDialog {
+	/**
+	 * CmrRepositoryDefinition.
+	 */
+	private CmrRepositoryDefinition cmrRepositoryDefinition;
+	/**
+	 * role name text box.
+	 */
+	private Text roleNameBox;
+
+	/**
+	 * Edit button.
+	 */
+	private Button editButton;
+	
+	/**
+	 * List of all Roles.
+	 */
+	private List<Role> rolesList;
+	
+	/**
+	 * The role to edit.
+	 */
+	private Role roleOld;
+	
+	/**
+	 * Reset button id.
+	 */
+	private static final int EDIT_ID = 0; //IDialogConstants.OK_ID;
+	/**
+	 * List of permissions that the current user can give to the new role.
+	 */
+	private List<String> grantedPermissions; 
+	/**
+	 * Array of buttons to display the permissions that can be granted.
+	 */
+	private Button[] grantedPermissionsButtons; 
+	/**
+	 * Default constructor.
+	 * 
+	 * @param parentShell
+	 *            Parent {@link Shell} to create Dialog on
+	 * @param cmrRepositoryDefinition
+	 * CmrRepositoryDefinition for easy access to security services.
+	 * @param role
+	 * the role to edit.
+	 */
+	public EditRoleDialog(Shell parentShell, CmrRepositoryDefinition cmrRepositoryDefinition, Role role) {
+		super(parentShell);
+		this.cmrRepositoryDefinition = cmrRepositoryDefinition;
+		rolesList = cmrRepositoryDefinition.getSecurityService().getAllRoles();
+		roleOld = role;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void create() {
+		super.create();
+		this.setTitle("Edit Role");
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		final Composite main = new Composite(parent, SWT.NONE);
+		main.setLayout(new GridLayout(2, false));
+		GridData gd = new GridData(SWT.FILL, SWT.FILL, true, true);
+		gd.widthHint = 400;
+		gd.heightHint = 100;
+		main.setLayoutData(gd);
+
+		Label roleNameBoxLabel = new Label(main, SWT.NONE);
+		roleNameBoxLabel.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
+		roleNameBoxLabel.setText("name:");
+		roleNameBox = new Text(main, SWT.BORDER);
+		roleNameBox.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		roleNameBox.setText(roleOld.getTitle());
+
+		Label textPermissionLabel = new Label(main, SWT.NONE);
+		textPermissionLabel.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false, 2, 5));
+		textPermissionLabel.setText("Mark the permissions, that the new role should have:");
+
+		this.grantedPermissions = cmrRepositoryDefinition.getGrantedPermissions();
+		this.grantedPermissionsButtons = new Button[grantedPermissions.size()];
+		for (int i = 0; i < grantedPermissions.size(); i++) {
+			grantedPermissionsButtons[i] = new Button(parent, SWT.CHECK);
+			grantedPermissionsButtons[i].setText(grantedPermissions.get(i));
+		}
+
+
+		return main;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+		editButton = createButton(parent, EDIT_ID, "Edit", true);
+		editButton.setEnabled(true);
+		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CLOSE_LABEL, false);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void buttonPressed(int buttonId) {
+		if (EDIT_ID == buttonId) {
+			editPressed();
+		} else if (IDialogConstants.CANCEL_ID == buttonId) {
+			cancelPressed();
+		}
+	}
+	
+	/**
+	 * Notifies that the edit button has been pressed.
+	 */
+	private void editPressed() {
+		okPressed();
+	}
+}
--- inspectIT/src/info/novatec/inspectit/rcp/dialog/ShowAllRolesDialog.java
+++ inspectIT/src/info/novatec/inspectit/rcp/dialog/ShowAllRolesDialog.java
@@ -1,16 +1,156 @@
 package info.novatec.inspectit.rcp.dialog;
 
+import java.util.List;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Display;
 
-public class ShowAllRolesDialog {
+import info.novatec.inspectit.communication.data.cmr.Permission;
+import info.novatec.inspectit.communication.data.cmr.Role;
+import info.novatec.inspectit.communication.data.cmr.User;
+import info.novatec.inspectit.rcp.repository.CmrRepositoryDefinition;
+import info.novatec.inspectit.rcp.wizard.page.CmrLoginWizardPage;
 
-	public ShowAllRolesDialog(Shell parentShell) {
-		// TODO Auto-generated constructor stub
+/**
+ * 
+ * @author Phil Szalay
+ *
+ */
+
+public class ShowAllRolesDialog extends TitleAreaDialog {
+	
+	/**
+	 * CmrRepositoryDefinition.
+	 */
+	private CmrRepositoryDefinition cmrRepositoryDefinition;
+	
+	/**
+	 * List of all Roles.
+	 */
+	private List<Role> roles;
+	/**
+	 * Table to display roles.
+	 */
+	private Table table;
+	/**
+	 * {@link EditRoleDialog}.
+	 */
+	private EditRoleDialog editRoleDialog;
+	/**
+	 * Default constructor.
+	 * @param parentShell
+	 * 				Parent {@link Shell} to create Dialog on
+	 * @param cmrRepositoryDefinition
+	 * CmrRepositoryDefinition for easy access to security services.
+	 */
+	public ShowAllRolesDialog(Shell parentShell, CmrRepositoryDefinition cmrRepositoryDefinition) {
+		super(parentShell);
+		this.cmrRepositoryDefinition = cmrRepositoryDefinition;
+		roles = cmrRepositoryDefinition.getSecurityService().getAllRoles();
 	}
-
-	public void open() {
-		// TODO Auto-generated method stub
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void create() {
+		super.create();
+		this.setTitle("Show all roles");
+	}
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		final Composite main = new Composite(parent, SWT.NONE);
+		main.setLayout(new GridLayout(2, false));
+		GridData gd = new GridData(SWT.FILL, SWT.FILL, true, true);
+		gd.widthHint = 400;
+		gd.heightHint = 100;
+		main.setLayoutData(gd);
 		
+		Label textLabel = new Label(main, SWT.NONE);
+		textLabel.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false, 2, 5));
+		textLabel.setText("All roles are shown below.");
+		
+		table = new Table(parent, SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION);
+		GridData gdTable = new GridData(SWT.FILL, SWT.FILL, true, true);
+		gdTable.heightHint = 200;
+		table.setLayoutData(gdTable);
+		table.setLinesVisible(true);
+		table.setHeaderVisible(true);
+		
+		// columns
+		TableColumn column1 = new TableColumn(table, SWT.NONE);
+		column1.setText("Role");
+		column1.pack();
+		TableColumn column2 = new TableColumn(table, SWT.NONE);
+		column2.setText("Permissions");
+		column2.pack();
+		
+		// content for rows
+		for (int i = 0; i < roles.size(); i++) {
+			TableItem item = new TableItem(table, SWT.NONE);
+			item.setText(0, roles.get(i).getTitle());
+			
+			List<Permission> permissions = roles.get(i).getPermissions();
+			String perm = "";
+			for (int k = 0; k < permissions.size(); k++) {
+				perm += permissions.get(k).getTitle() + ", ";
+			}
+			
+			item.setText(1, perm);
+		}
+		for (TableColumn column : table.getColumns()) {
+			column.pack();
+		}
+		table.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				if (table.getSelectionIndex() != -1) {
+					TableItem[] tableItems = table.getItems();
+					long id = 0;
+					for (Role r : roles) {
+				    	if (r.getTitle().equals(tableItems[table.getSelectionIndex()].getText(0))) {
+				    		id = r.getId();
+				    	}
+				    }
+					Role role = cmrRepositoryDefinition.getSecurityService().getRoleByID(id);
+					editRoleDialog(main.getShell(), role);
+				}
+			}
+		});
+		parent.pack();
+		
+		return main;
+	}
+	
+	/**
+	 * Dialog in case a user is clicked in the table.
+	 * 
+	 * @param parentShell
+	 *            parent shell for the {@link EditUserDialog}
+	 * @param user
+	 * 		 	  the user to edit.
+	 */
+	private void editRoleDialog(Shell parentShell, Role role) {
+		editRoleDialog = new EditRoleDialog(parentShell, cmrRepositoryDefinition, role);
+		editRoleDialog.open();
 	}
 
 }
