c896fd82b914a87c5a3e62d7e61f49255e8a5c42
Ivan Senic <ivan.senic@novatec-gmbh.de>
2016-08-17 18:22:30 +0200

    INSPECTIT-2185: Sorting by id and min duration filtering in the invocation overview restful method

--- inspectit.shared.all/src/main/java/rocks/inspectit/shared/all/communication/comparator/TimerDataComparatorEnum.java
+++ inspectit.shared.all/src/main/java/rocks/inspectit/shared/all/communication/comparator/TimerDataComparatorEnum.java
@@ -2,6 +2,8 @@ package rocks.inspectit.shared.all.communication.comparator;
 
 import java.util.Comparator;
 
+import com.google.common.primitives.Longs;
+
 import rocks.inspectit.shared.all.cmr.service.ICachedDataService;
 import rocks.inspectit.shared.all.communication.data.TimerData;
 
@@ -97,7 +99,7 @@ public enum TimerDataComparatorEnum implements IDataComparator<TimerData>, Compa
 		switch (this) {
 		case COUNT:
 			// Java5 does not have Long.compare
-			return Long.valueOf(o1.getCount()).compareTo(Long.valueOf(o2.getCount()));
+			return Longs.compare(o1.getCount(), o2.getCount());
 		case AVERAGE:
 			return Double.compare(o1.getAverage(), o2.getAverage());
 		case MIN:
