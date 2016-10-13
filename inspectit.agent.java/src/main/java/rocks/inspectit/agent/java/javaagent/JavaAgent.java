2af42d7a9ff74072a7252e54685b0c0ac8192405
Ivan Senic <ivan.senic@novatec-gmbh.de>
2016-10-13 15:57:53 +0200

    INSPECTIT-2169: JMX sensor updates - options to deactivate sensor, force MBean server creation, support for multiple servers monitoring

--- inspectit.agent.java/src/main/java/rocks/inspectit/agent/java/javaagent/JavaAgent.java
+++ inspectit.agent.java/src/main/java/rocks/inspectit/agent/java/javaagent/JavaAgent.java
@@ -35,6 +35,7 @@ import java.util.regex.Pattern;
 import rocks.inspectit.agent.java.Agent;
 import rocks.inspectit.agent.java.IAgent;
 import rocks.inspectit.agent.java.hooking.IHookDispatcher;
+import rocks.inspectit.agent.java.sensor.jmx.IMBeanServerListener;
 
 /**
  * The JavaAgent is used since Java 5.0 to instrument classes before they are actually loaded by the
@@ -120,6 +121,7 @@ public class JavaAgent implements ClassFileTransformer {
 	/**
 	 * {@inheritDoc}
 	 */
+	@Override
 	public byte[] transform(ClassLoader classLoader, String className, Class<?> clazz, ProtectionDomain pd, byte[] data) throws IllegalClassFormatException {
 		boolean threadTransformDisabled = Agent.agent.isThreadTransformDisabled();
 		if (threadTransformDisabled) {
@@ -364,6 +366,7 @@ public class JavaAgent implements ClassFileTransformer {
 
 			// ignore IAgent because this is the interface for the SUD to access the real agent
 			ignoreClasses.add(IAgent.class.getCanonicalName());
+			ignoreClasses.add(IMBeanServerListener.class.getCanonicalName());
 			ignoreClasses.add(Agent.class.getCanonicalName());
 
 			// ignore hook dispatcher because it is defined in the IAgent interface and thus must be
