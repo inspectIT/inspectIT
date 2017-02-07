50fdd271b6f4be4f1aa5c77277cfb187f4c281f2
Marius Oehler <marius.oehler@novatec-gmbh.de>
2017-02-07 19:44:09 +0100

    INSPECTIT-2323: inspectIT UI throws exception during alert creating when using InfluxDB v1.2.0

--- inspectit.server/src/main/java/rocks/inspectit/server/influx/util/InfluxClientFactory.java
+++ inspectit.server/src/main/java/rocks/inspectit/server/influx/util/InfluxClientFactory.java
@@ -1,5 +1,6 @@
 package rocks.inspectit.server.influx.util;
 
+import org.apache.commons.lang.StringUtils;
 import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDBFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -46,8 +47,11 @@ public class InfluxClientFactory {
 	 *             is thrown if the client could not be created
 	 */
 	public InfluxDB createClient() throws IllegalArgumentException {
-		if (host == null) {
-			throw new IllegalArgumentException("Host may not be null.");
+		if (StringUtils.isEmpty(host)) {
+			throw new IllegalArgumentException("Host may not be null or empty.");
+		}
+		if (port <= 0) {
+			throw new IllegalArgumentException("Specify a valid port in the range between 1 and 65535.");
 		}
 		String influxUrl = "http://" + host + ":" + port;
 		return InfluxDBFactory.connect(influxUrl, user, password);
